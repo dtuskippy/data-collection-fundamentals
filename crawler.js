d8f3cd85163c3b733f005b66edb124c56c0b7b3b
Alan Chelko <achelko@gmail.com>
2023-09-23 19:19:26 +0000

    Added crawler.js

---
+++ crawler.js
@@ -0,0 +1,16 @@
+import * as cheerio from "cheerio";
+import { gotScraping } from 'got-scraping';
+
+const storeUrl = 'https://www.espn.com/';
+// const storeUrl = 'https://warehouse-theme-metal.myshopify.com/collections/sales';
+
+const response = await gotScraping(storeUrl);
+const html = response.body;
+
+const $ = cheerio.load(html);
+// the cheerio.load(html) is effectively document.querySelectorAll ...
+const links = $('a');
+for (const link of links) {
+  let url = $(link).attr('href');
+  console.log(url);
+}
